9e1e112b275257ed94ee6a74dcb205b542d20863
Chris Hickey <chrismhickey@gmail.com>
2024-11-25 12:36:52 -0500

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,16 @@
-def fizzbuzz(n)
-    (1..n).each do |i|
-      if i % 3 == 0 && i % 5 == 0
-        puts 'fizzbuzz'
-      elsif i % 3 == 0
-        puts 'fizz'
-      elsif i % 5 == 0
-        puts 'buzz'
-      else
-        puts i
-      end
-    end
+def fizzbuzz(number)
+  return "FizzBuzz" if number % 15 == 0
+  return "Fizz" if number % 3 == 0
+  return "Buzz" if number % 5 == 0
+  nil
+end
+
+# Wrapper method to handle a range and print results
+def fizzbuzz_printer(n)
+  (1..n).each do |i|
+    puts fizzbuzz(i) || i
   end
-  
-  fizzbuzz(100)
+end
+
+# Call fizzbuzz_printer for numbers 1 to 100
+fizzbuzz_printer(100)
